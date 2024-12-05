aa2a67dc858c48d0770e5621f11d0c8319e8f656
Mateusz Domino <mdomino60@gmail.com>
2024-12-05 16:25:47 +0100

    Initialize project structure

---
+++ src/main/java/pl/mdomino/artapp/ArtAppApplication.java
@@ -0,0 +1,13 @@
+package pl.mdomino.artapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtAppApplication.class, args);
+	}
+
+}
