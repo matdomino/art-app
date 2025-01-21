a8f9c8072d81efb8ec223d79a807ce4c755a5820
Mateusz Domino <mdomino60@gmail.com>
2025-01-21 02:44:46 +0100

    Add search page, update header for every page

--- art-app/src/main/java/pl/mdomino/artapp/web/controller/WebImageController.java
+++ art-app/src/main/java/pl/mdomino/artapp/web/controller/WebImageController.java
@@ -7,6 +7,8 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.mdomino.artapp.model.Image;
 import pl.mdomino.artapp.model.dto.ImageDTO;
 import pl.mdomino.artapp.service.ImageService;
 
@@ -24,11 +26,6 @@ public class WebImageController {
 
     @GetMapping("/")
     public String getIndex(Model model, Authentication auth) {
-        if (auth != null) {
-            System.out.println("=== Authorities: ===");
-            auth.getAuthorities().forEach(a -> System.out.println(a.getAuthority()));
-        }
-
         model.addAttribute("name",
                 auth instanceof OAuth2AuthenticationToken oauth && oauth.getPrincipal() instanceof OidcUser oidc
                         ? oidc.getPreferredUsername()
@@ -48,11 +45,53 @@ public class WebImageController {
 
     @GetMapping("/test")
     public String test(Model model, Authentication auth) {
+        model.addAttribute("name",
+                auth instanceof OAuth2AuthenticationToken oauth && oauth.getPrincipal() instanceof OidcUser oidc
+                        ? oidc.getPreferredUsername()
+                        : "");
+        model.addAttribute("isAuthenticated",
+                auth != null && auth.isAuthenticated());
         return "test.html";
+
     }
 
     @GetMapping("/admin")
     public String adminPage(Model model, Authentication auth) {
+        model.addAttribute("name",
+                auth instanceof OAuth2AuthenticationToken oauth && oauth.getPrincipal() instanceof OidcUser oidc
+                        ? oidc.getPreferredUsername()
+                        : "");
+        model.addAttribute("isAuthenticated",
+                auth != null && auth.isAuthenticated());
         return "admin.html";
+    }
+
+    @GetMapping("/search")
+    public String search(@RequestParam("q") String query,
+                         @RequestParam(defaultValue = "uploadDate") String sortBy,
+                         @RequestParam(defaultValue = "false") boolean ascending,
+                         @RequestParam(defaultValue = "0") int page,
+                         @RequestParam(defaultValue = "10") int size,
+                         Model model, Authentication auth) {
+        model.addAttribute("name",
+                auth instanceof OAuth2AuthenticationToken oauth && oauth.getPrincipal() instanceof OidcUser oidc
+                        ? oidc.getPreferredUsername()
+                        : "");
+        model.addAttribute("isAuthenticated",
+                auth != null && auth.isAuthenticated());
+        model.addAttribute("query", query);
+
+        model.addAttribute("ascending", ascending);
+        model.addAttribute("sortBy", sortBy);
+        model.addAttribute("page", page);
+        model.addAttribute("size", size);
+        model.addAttribute("q", query);
+
+        System.out.println(page);
+
+        List<Image> images = imageService.searchImages(query, sortBy, ascending, page, size);
+        model.addAttribute("images", images);
+
+        return "search.html";
     }
 }
